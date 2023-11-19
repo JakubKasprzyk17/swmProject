91e53f6ae2fc562a58837fbd371c3ffb3cb77a0b
Jakub Kasprzyk <j.kasprzyk@mymusic.pl>
2023-11-19 21:58:01 +0100

    Add Typography

---
+++ src/styles/typography.ts
@@ -0,0 +1,14 @@
+export const FONT_FAMILY_BOLD = 'Ubuntu-Bold' as const;
+export const FONT_FAMILY_LIGHT = 'Ubuntu-Light' as const;
+export const FONT_FAMILY_MEDIUM = 'Ubuntu-Medium' as const;
+export const FONT_FAMILY_REGULAR = 'Ubuntu-Regular' as const;
+
+export const FONT_SIZE_10 = 10;
+export const FONT_SIZE_11 = 11;
+export const FONT_SIZE_12 = 12;
+export const FONT_SIZE_13 = 13;
+export const FONT_SIZE_14 = 14;
+export const FONT_SIZE_15 = 15;
+export const FONT_SIZE_16 = 16;
+export const FONT_SIZE_20 = 20;
+export const FONT_SIZE_24 = 24;
