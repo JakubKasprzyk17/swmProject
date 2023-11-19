aa11a40665eebf7304ade882d96db1e22a12e073
Jakub Kasprzyk <j.kasprzyk@mymusic.pl>
2023-11-19 22:16:22 +0100

    Add Artwork Details

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
+export { default as ArtworkDetails } from './ArtworkDetails/ArtworkDetails';
 export { default as Explore } from './Explore/Explore';
 export { default as Favourite } from './Favourite/Favourite';
 export { default as Search } from './Search/Search';
